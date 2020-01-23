4a7cb6db5456cfc15f6b05d542630d1a40781e5d
Mark Stout <markspcs@yahoo.com>
2020-01-22 21:12:26 -0600

    added all functions

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ const nflArrestSite = "http://nflarrest.com/api/v1/player/arrests/";
 const nflTeamSite = "http://www.markstout.com/api/nfl/teams";
 const nflPlayerSite = "http://www.markstout.com/api/nfl/players"
 var playerName = "Aldon%20Smith";
-var teamName = "KC";
-var playerId = "3311"
+//var teamName = "KC";
+//var playerId = "3311"
 getTeams();
-getTeamPlayers(teamName);
-getPlayer(playerId);
-//getArrestInfo()
+
+$(document).on("click", ".teams", function () {
+    getTeamPlayers($(this).attr("team-name"))
+});
+$(document).on("click", ".team", function () {
+    getPlayer($(this).attr("player-code"));
+});
+$(document).on("click", ".player", function () {
+    getArrestInfo($(this).attr("player-name"));
+})
+//getTeamPlayers(teamName);
+//getPlayer(playerId);
+getArrestInfo()
 
 ///////////////////////////////////////////////////////////
 //get a single players info using the ID in the api
@@ -36,7 +46,14 @@ function getPlayer(playerId) {
                     $("#teamcontainer").append(playerDiv);
                     playerDiv.attr("player-name", player.displayName);
                     playerDiv.append(player.displayName);
-                    //playersUl.append("<li class='list-player-item' player-name='" + player.displayName + "'>" + player.displayName + "<br>" + player.jersey + player.height + player.dob + "</li>");
+                    let statDiv = $("<div>").addClass("playerStat");
+                    $("#teamcontainer").append(statDiv);
+                    statDiv.append("<div class='jersey'>Number: " + player.jersey + "</div>");
+                    statDiv.append("<div class='position'>Position: " + player.position + "</div>");
+                    statDiv.append("<div class='height'>Height: " + player.height + "</div>");
+                    statDiv.append("<div class='weight'>Weight: " + player.weight + "</div>");
+                    statDiv.append("<div class='college'>College: " + player.college + "</div>");
+                    statDiv.append("<div class='DOB'>DOB: " + player.dob + "</div>");
                 }
             })
         })
@@ -46,6 +63,7 @@ function getPlayer(playerId) {
 //list of players per team. Team is passed as the argument
 function getTeamPlayers(team) {
     console.log(team);
+    console.log("here");
     $.get({
         url: nflPlayerSite,
         method: "GET",
@@ -57,7 +75,7 @@ function getTeamPlayers(team) {
             //let playersUl = $('<ul/>').addClass('list-players');
             //$("body").append(playersUl);
             response.Players.forEach(function (player) {
-                if (player.team === teamName) {
+                if (player.team === team) {
                     console.log(player);
                     let playersDiv = $("<div>").addClass("team");
                     $("#teamcontainer").append(playersDiv);
@@ -107,7 +125,8 @@ function getTeams() {
 }
 ////////////////////////////////////////////////////////
 // function to get the arrest info, and update divs
-function getArrestInfo() {
+function getArrestInfo(playerName) {
+    console.log(playerName);
     let arrestAjax = $.ajax({
         url: nflArrestSite + playerName,
         method: "GET",
@@ -115,6 +134,18 @@ function getArrestInfo() {
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
             console.log(response);
+            let guiltyDiv = $("<div>").addClass("guilty");
+            $("#teamcontainer").append(guiltyDiv);
+            //plaDiv.attr("player-name", player.displayName);
+            guiltyDiv.append(response[0].Name + response[0].Team_name + response[0].Category + response[0].Description + response[0].Outcome);
+            // let statDiv = $("<div>").addClass("playerStat");
+            // $("#teamcontainer").append(statDiv);
+            // statDiv.append("<div class='jersey'>Number: " + player.jersey + "</div>");
+            // statDiv.append("<div class='position'>Position: " + player.position + "</div>");
+            // statDiv.append("<div class='height'>Height: " + player.height + "</div>");
+            // statDiv.append("<div class='weight'>Weight: " + player.weight + "</div>");
+            // statDiv.append("<div class='college'>College: " + player.college + "</div>");
+            // statDiv.append("<div class='DOB'>DOB: " + player.dob + "</div>");
 
         })
 
