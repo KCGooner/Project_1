6d3cd3a19bb5c51efe2ce31c1dc72d8e59b81baa
Mark Stout <markspcs@yahoo.com>
2020-01-22 19:06:38 -0600

    added list of teams

--- script.js
+++ script.js
@@ -10,90 +10,96 @@ const nflPlayerSite = "http://www.markstout.com/api/nfl/players"
 var playerName = "Aldon%20Smith";
 var teamName = "KC";
 var playerId = "3311"
-//getTeams();
-getTeamPlayers(teamName);
-getPlayer(playerId);
-getArrestInfo()
+getTeams();
+//getTeamPlayers(teamName);
+//getPlayer(playerId);
+//getArrestInfo()
 
 ///////////////////////////////////////////////////////////
 //get a single players info using the ID in the api
-function getPlayer(playerId){
-  console.log(playerId);
-  $.get({
-    url: nflPlayerSite,
-    method: "GET",
-    dataType: 'json'
-  })
-  .then(function (response) {
-    console.log(response);
-    let playersUl = $('<ul/>').addClass('list-player');
-    $("body").append(playersUl);
-      response.Players.forEach(function(player){
-        if(player.playerId === playerId){
-          console.log(player);
-          playersUl.append("<li class='list-player-item' player-name='" + player.displayName + "'>" + player.displayName + "<br>" + player.jersey + player.height + player.dob + "</li>");  
-        }
-      })
-     })
+function getPlayer(playerId) {
+    console.log(playerId);
+    $.get({
+        url: nflPlayerSite,
+        method: "GET",
+        dataType: 'json'
+    })
+        .then(function (response) {
+            console.log(response);
+            let playersUl = $('<ul/>').addClass('list-player');
+            $("body").append(playersUl);
+            response.Players.forEach(function (player) {
+                if (player.playerId === playerId) {
+                    console.log(player);
+                    playersUl.append("<li class='list-player-item' player-name='" + player.displayName + "'>" + player.displayName + "<br>" + player.jersey + player.height + player.dob + "</li>");
+                }
+            })
+        })
 }
 
 ///////////////////////////////////////////////////////////
 //list of players per team. Team is passed as the argument
-function getTeamPlayers(team){
-  console.log(team);
-  $.get({
-    url: nflPlayerSite,
-    method: "GET",
-    dataType: 'json'
-  })
-  .then(function (response) {
-    console.log(response);
-    let playersUl = $('<ul/>').addClass('list-players');
-    $("body").append(playersUl);
-      response.Players.forEach(function(player){
-        if(player.team === teamName){
-          console.log(player);
-          playersUl.append("<li class='list-players-item' player-code='" + player.playerId + "'>" + player.displayName + "</li>");  
-        }
-      })
-     })
+function getTeamPlayers(team) {
+    console.log(team);
+    $.get({
+        url: nflPlayerSite,
+        method: "GET",
+        dataType: 'json'
+    })
+        .then(function (response) {
+            console.log(response);
+            let playersUl = $('<ul/>').addClass('list-players');
+            $("body").append(playersUl);
+            response.Players.forEach(function (player) {
+                if (player.team === teamName) {
+                    console.log(player);
+                    playersUl.append("<li class='list-players-item' player-code='" + player.playerId + "'>" + player.displayName + "</li>");
+                }
+            })
+        })
 }
 
 ///////////////////////////////////////////////////////////
 //Function to get team info and update divs
-function getTeams(){
-  console.log("getTeams");
-  $.get({
-    url: nflTeamSite,
-    method: "GET",
-    dataType: 'json'
-  })
-  .then(function (response) {
-    console.log(response);
-    let teamUl = $('<ul/>').addClass('list-team');
-    $("body").append(teamUl);
-    console.log(response.NFLTeams.length);
-    response.NFLTeams.forEach(function(team){
-      console.log(team.fullName);
-      teamUl.append("<li class='list-team-item' team-code='" + team.code + "'>" + team.fullName + "</li>");
+function getTeams() {
+    console.log("getTeams");
+    $.get({
+        url: nflTeamSite,
+        method: "GET",
+        dataType: 'json'
     })
-    
+        .then(function (response) {
+            console.log(response);
+            //let teamUl = $('<ul/>').addClass('list-team');
+            //$("body").append(teamUl);
 
-  })
+            console.log(response.NFLTeams.length);
+            response.NFLTeams.forEach(function (team) {
+                console.log(team.fullName);
+                let teamDiv = $("<div>").addClass("teams");
+                $("#teamcontainer").append(teamDiv);
+                teamDiv.attr("team-name", team.code);
+                teamDiv.append(team.fullName);
+
+                //teamUl.append("<li class='list-team-item' team-code='" + team.code + "'>" + team.fullName + "</li>");
+            })
+
+
+        })
 }
 ////////////////////////////////////////////////////////
 // function to get the arrest info, and update divs
 function getArrestInfo() {
     let arrestAjax = $.ajax({
-      url: nflArrestSite + playerName,
-      method: "GET",
+        url: nflArrestSite + playerName,
+        method: "GET",
     })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function (response) {
-        console.log(response);
-        
-      })
-  
-  }
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function (response) {
+            console.log(response);
+
+        })
+
+}
 
 
